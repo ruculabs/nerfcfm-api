e048afdcc46b85dc9143d399e22bd42aa608fed7
iPolarisu <elpolarisu@gmail.com>
2023-12-01 22:20:36 -0300

    feat: add error print on model creation exception

--- api/views.py
+++ api/views.py
@@ -146,8 +146,12 @@ class GenerateNerfModelView(generics.CreateAPIView):
                 )
                 nerf_model.save()
 
-            except:
-
+            except Exception as err:
+                # internal error
+                print('[GENERATE_NERF_MODEL]: Create Model Exception')
+                print('-- GENERATE_NERF_MODEL EXCEPTION START --')
+                print(err)
+                print('-- GENERATE_NERF_MODEL EXCEPTION END --')
                 return Response(
                     {'message': 'Error creating nerf_model, try later'}, 
                     status=status.HTTP_500_INTERNAL_SERVER_ERROR)
