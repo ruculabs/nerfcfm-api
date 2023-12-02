cfcfcfeb897224ebd4400d7b9b212c151c581007
iPolarisu <elpolarisu@gmail.com>
2023-12-01 23:37:24 -0300

    chore: add exportmethod to admin

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Video, Nerf, NerfModel, NerfObject
+from .models import Video, ExportMethod, Nerf, NerfModel, NerfObject
 
 admin.site.register(Video)
 admin.site.register(Nerf)
+admin.site.register(ExportMethod)
 admin.site.register(NerfModel)
 admin.site.register(NerfObject)
