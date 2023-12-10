500764fcc70c8b602f2007c55d7b5999625825af
iPolarisu <elpolarisu@gmail.com>
2023-12-10 20:26:01 -0300

    chore: add data to admin

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Video, ExportMethod, Nerf, NerfModel, NerfObject
+from .models import Video, ExportMethod, Data, DataType, Nerf, NerfModel, NerfObject
 
 admin.site.register(Video)
+admin.site.register(Data)
+admin.site.register(DataType)
 admin.site.register(Nerf)
 admin.site.register(ExportMethod)
 admin.site.register(NerfModel)
