0ed9df550214d74e2b8b9e84cebc78aa1d345b77
iPolarisu <elpolarisu@gmail.com>
2023-12-10 21:54:28 -0300

    chore: add all data types url

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import UserRegistrationView, UserLoginView
 from .views import VideoUploadView, UserVideosView
+from .views import AllDataTypesView
 from .views import AllNerfsView
 from .views import GenerateNerfModelView, UserNerfModelsView
 from .views import GenerateNerfObjectView, UserNerfObjectsView
@@ -15,14 +16,19 @@ urlpatterns = [
     path('video-upload/', VideoUploadView.as_view(), name='video-upload'),
     path('user-videos/', UserVideosView.as_view(), name='user-videos'),
 
+    # data types
+    path('all-data-types/', AllDataTypesView.as_view(), name='all-data-types'),
+
+    # data
+
     # nerfs
     path('all-nerfs/', AllNerfsView.as_view(), name='all-nerfs'),
 
-    # models
+    # nerf models
     path('generate-nerf-model/', GenerateNerfModelView.as_view(), name='generate-nerf-model'),
     path('user-nerf-models/', UserNerfModelsView.as_view(), name='user-nerf-models'),
 
-    # objects
+    # nerf objects
     path('generate-nerf-object/', GenerateNerfObjectView.as_view(), name='generate-nerf-object'),
     path('user-nerf-objects/', UserNerfObjectsView.as_view(), name='user-nerf-objects'),
     
