87b09ecea82055c824941e13a3865aa5e2bc87c2
iPolarisu <elpolarisu@gmail.com>
2023-12-17 17:40:51 -0300

    chore: change video to data & data to processed

--- api/models.py
+++ api/models.py
@@ -2,16 +2,18 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-class Video(models.Model):
+class Data(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    video_file = models.FileField(upload_to='videos/')
+
+    data_file = models.FileField(upload_to='data/')
+    data_type = models.ForeignKey(DataType, on_delete=models.CASCADE)
+
     name = models.CharField(max_length=255)
+    description = models.TextField(default='')
     upload_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
-
-### Video -> Data
 
 class DataType(models.Model):
     name = models.CharField(max_length=100, default='')
@@ -22,10 +24,10 @@ class DataType(models.Model):
     def __str__(self):
         return self.name
 
-class Data(models.Model):
+class ProcessedData(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    video = models.ForeignKey(Video, on_delete=models.CASCADE)
-    data_type = models.ForeignKey(DataType, on_delete=models.CASCADE)
+    data = models.ForeignKey(Data, on_delete=models.CASCADE)
+
     STATUS_DATA_CHOICES = [
         ('in_progress', 'In Progress'),
         ('complete', 'Complete'),
@@ -33,14 +35,22 @@ class Data(models.Model):
     ]
     status = models.CharField(max_length=255, choices=STATUS_DATA_CHOICES)
     data_file = models.FileField(upload_to='data/')
+    
     start_date = models.DateField()
     end_date = models.DateField()
-    creation_time = models.DurationField()
+    processing_time = models.DurationField()
+
+    def save(self, *args, **kwargs):
+        if self.start_date and self.end_date:
+            self.processing_time = self.end_date - self.start_date
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.video.name}{self.data_type}Name"
 
-# Data -> NerfModel
+    def save_endtime(self):
+        self.end_date = timezone.now()
+        self.save()
 
 class Nerf(models.Model):
     name = models.CharField(max_length=50, default='')
@@ -54,8 +64,10 @@ class Nerf(models.Model):
 class NerfModel(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     data = models.ForeignKey(Data, on_delete=models.CASCADE)
+
     model_file = models.FileField(upload_to='nerf_models/')
     nerf = models.ForeignKey(Nerf, on_delete=models.CASCADE)
+
     start_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField(null=True, blank=True)
     training_time = models.DurationField(null=True, blank=True)
@@ -92,10 +104,12 @@ class ExportMethod(models.Model):
 class NerfObject(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     nerf_model = models.ForeignKey(NerfModel, on_delete=models.CASCADE)
+    
     object_file = models.FileField(upload_to='nerf_objects/')
+    
     start_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField(null=True, blank=True)
-    creation_time = models.DurationField(null=True, blank=True)
+    export_time = models.DurationField(null=True, blank=True)
 
     STATUS_OBJECT_CHOICES = [
         ('in_progress', 'In Progress'),
@@ -106,7 +120,7 @@ class NerfObject(models.Model):
 
     def save(self, *args, **kwargs):
         if self.start_date and self.end_date:
-            self.creation_time = self.end_date - self.start_date
+            self.export = self.end_date - self.start_date
         super().save(*args, **kwargs)
     
     def save_endtime(self):
@@ -121,12 +135,18 @@ class NerfObject(models.Model):
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     data = models.ForeignKey(Data, on_delete=models.CASCADE)
+
+    processed_data = models.ForeignKey(ProcessedData, on_delete=models.CASCADE)
     nerf_model = models.ForeignKey(NerfModel, on_delete=models.CASCADE)
     nerf_object = models.ForeignKey(NerfObject, on_delete=models.CASCADE)
+
+    fidelity_rating = models.PositiveIntegerField(default=0, choices=[(i, i) for i in range(1, 6)], verbose_name='Fidelity Rating')
     detail_rating = models.PositiveIntegerField(default=0, choices=[(i, i) for i in range(1, 6)], verbose_name='Detail Rating')
     definition_rating = models.PositiveIntegerField(default=0, choices=[(i, i) for i in range(1, 6)], verbose_name='Definition Rating')
     usability_rating = models.PositiveIntegerField(default=0, choices=[(i, i) for i in range(1, 6)], verbose_name='Usability Rating')
+    
     comment = models.TextField(blank=True)
+    
     date_posted = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
