e5adc48335318ad55cf2a5e8ed58e373d5d37934
iPolarisu <elpolarisu@gmail.com>
2023-12-17 17:51:59 -0300

    chore: update admin models data & processed

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Video, ExportMethod, Data, DataType, Nerf, NerfModel, NerfObject
+from .models import Data, ExportMethod, ProcessedData, DataType, Nerf, NerfModel, NerfObject
 
-admin.site.register(Video)
 admin.site.register(Data)
+admin.site.register(ProcessedData)
 admin.site.register(DataType)
 admin.site.register(Nerf)
 admin.site.register(ExportMethod)
