262044dc9ce6cdd711270aca2fd498405f13d514
Sebastián Saavedra <elpolarisu@gmail.com>
2024-04-24 22:37:38 -0400

    chore(docs): create docs folder and add drf-spectacular for API

--- nerfcfm/urls.py
+++ nerfcfm/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
+    # drf_spectacular urls
+    path('api/schema', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(), name='redoc'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    # Admin
     path('admin/', admin.site.urls),
+    # API
     path('api/', include('api.urls'))
 ]
